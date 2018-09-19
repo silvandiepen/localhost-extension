99e7e27d23969c66c1a5b234c3e15a6630205258
Sil <sil@matise.nl>
2018-09-19 16:27:56 +0200

    add new checking of max chars

--- background.js
+++ background.js
@@ -27,16 +27,27 @@ var convertToNumbers = function(insert) {
 };
 var navigate = function(url) {
 	chrome.tabs.getSelected(null, function(tab) {
+		console.log(tab);
 		chrome.tabs.update(tab.id, { url: url });
 	});
 };
 
 chrome.runtime.onInstalled.addListener(function() {
-	chrome.omnibox.onInputChanged.addListener(function(e) {
-		console.log('changed to', e);
+	chrome.omnibox.onInputChanged.addListener(function(value) {
+		console.log('changed to', value);
 	});
-	chrome.omnibox.onInputEntered.addListener(function(e) {
-		var url = 'http://localhost:' + convertToNumbers(e);
+	chrome.omnibox.onInputEntered.addListener(function(value) {
+		let port = value;
+		// Limit to 4 numbers
+		if (port.length > 4) {
+			port = port.substring(0, 4);
+			console.log('localhost: too long, so shortened and kept at 4 numbers');
+		} else if (port.length < 4) {
+			alert("ports with less then 4 numbers won't work");
+		}
+		//
+		var url = 'http://localhost:' + convertToNumbers(port);
+		console.log(url);
 		navigate(url);
 	});
 });
